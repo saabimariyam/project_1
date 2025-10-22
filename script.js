6a752b184229e97da844d4cbf26016ed2675e07c
saabimariyam <saabichaabu@gmail.com>
2025-10-22 14:29:41 +0530

    completed

--- script.js
+++ script.js
@@ -11,3 +11,15 @@ menuBtn.addEventListener("click",() => {
 closeMenuBtn.addEventListener("click",() => {
     menuBtn.click();
 });
+
+// fade animation
+const fadeElements = document.querySelectorAll('.fade-in');
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+        }
+    });
+});
+
+fadeElements.forEach(el => observer.observe(el));
