5b03af6e19941a7b6905d816636495466edb4a74
saabimariyam <saabichaabu@gmail.com>
2025-10-22 11:43:55 +0530

    complete

---
+++ script.js
@@ -0,0 +1,13 @@
+const header = document.querySelector("header");
+const menuBtn = document.querySelector("#menu-btn");
+const closeMenuBtn = document.querySelector("#close-menu-btn");
+
+// toggle mobile menu on menu button click
+menuBtn.addEventListener("click",() => {
+    header.classList.toggle("show-mobile-menu");
+});
+
+// close mobile menu on close button click
+closeMenuBtn.addEventListener("click",() => {
+    menuBtn.click();
+});
